90a62ea8f0d0b5a89cf135a4b5bb04a8558c05a0
PierreVelluet <velluetp@gmail.com>
2020-04-11 18:26:41 +0200

    job done

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Home.module.css'
+
+const Home = () => {
+    return (
+        <div>
+            <div className={styles.BackgroundImage} ></div>
+            <div className={styles.Title} ><h1>In My Fridge</h1></div>
+        </div>
+    )
+}
+
+export default Home;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import styles from './Navbar.module.css';
+import { HashLink as Link } from 'react-router-hash-link';
+import LogoFrigo from '../Assets/LogoFrigo/LogoFrigo';
 
 const Navbar = () => {
     return (
-        <div className={styles.Navbar}>
-            <h1>Empty My Fridge</h1>
+        <nav className={styles.Navbar}>
+        <div className={styles.LogoAndTitleContainer} >
+            <LogoFrigo width={'75px'} height={'75px'}/>
+            <h2>Empty your fridge</h2>
         </div>
+        
+            <div className={styles.NavItemsContainer}>
+                <div className={styles.NavItems}><Link smooth to='/home'>Home</Link></div>
+                <div className={styles.NavItems}><Link smooth to='/inMyFridge'>In my fridge</Link></div>
+                <div className={styles.NavItems}><Link smooth to='/aboutUs'>About us</Link></div>
+                <div className={styles.NavItems}><Link smooth to='/contactUs'>Contact us</Link></div>
+            </div>
+        </nav>
     )
 }
 
