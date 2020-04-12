220f80721fb5d74a28af9f037836fe88e10f2812
PierreVelluet <velluetp@gmail.com>
2020-04-12 17:42:10 +0200

    homepage done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
-import { BrowserRouter } from 'react-router-dom'
-import { Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Redirect} from 'react-router-dom';
 import Home from './components/Home/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+          <Redirect exact from="/" to="/home" />
           <Navbar />
           <Route path='/home' exact component={Home}/>
           <Footer />
