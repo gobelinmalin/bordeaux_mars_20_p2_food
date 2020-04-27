8bcfc51f162ab44eee0047026f12cf93c9047c9e
PierreVelluet <velluetp@gmail.com>
2020-04-27 10:08:27 +0200

    minor changes

--- src/components/InMyFridge/RecipeList/RecipeModal/RecipeModal.js
+++ src/components/InMyFridge/RecipeList/RecipeModal/RecipeModal.js
@@ -139,16 +139,11 @@ export default function TransitionsModal(props) {
                   </div>
                   <div>
                     <img src="../../../../Images/Icone/icon-prep-time@2x.png"alt="icone-cooking-min"/>
-                    {console.log(recipe.map(element => element.preparationMinutes))}
                     {recipe.map(element => element.preparationMinutes !== undefined
                     ? <small> Prep {recipe.map(element => element.preparationMinutes)} min</small>
                     : <small>no prep. time</small>
                     )}
                   </div>
-
-                  <div><img src="../../../../Images/Icone/icon-cook-time@2x.png"alt="icone-cooking-min"/><small> Cook {recipe.map(element => element.cookingMinutes)} min</small></div>
-                  <div><img src="../../../../Images/Icone/icon-people-count@2x.png"alt="icone-cooking-min"/><small> For {recipe.map(element => element.servings)} people</small></div>
-                  <div><img src="../../../../Images/Icone/icon-prep-time@2x.png"alt="icone-cooking-min"/><small> Prep {recipe.map(element => element.preparationMinutes)} min</small></div>
 
                 </div>
 
@@ -189,19 +184,14 @@ export default function TransitionsModal(props) {
                   {recipe.map(element => element.analyzedInstructions.length > 0 
                   ? recipe.map(element => element.analyzedInstructions.map(element2 => element2.steps.map(element3 => <li>{element3.step}</li>)))
                   : <p className={styles.noSpecialInfo}>No special instructions</p>
-                  )
-                  {recipe.map(element => element.analyzedInstructions
-                      .map(element2 => element2.steps
-                      .map(element3 => <li>{element3.step}</li>))
-                      )  
-                  }
+                  )}
+
                 </ol>
                 <h2 className={styles.title2} >Diets</h2>
                 <ul className={styles.orderList}>
                   {recipe.map(element => element.diets.length > 0
                   ? recipe.map(element => element.diets.map(diet => <li>{diet}</li>))
                   : <p className={styles.noSpecialInfo}>No special diets</p>)}
-                  {recipe.map(element => element.diets.length > 0 ? recipe.map(element => element.diets.map(diet => <li>{diet}</li>)) : <p>No special diets</p>)}                  
                 </ul>
               </div>
             </div>
