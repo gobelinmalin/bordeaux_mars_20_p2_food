632077e8c5a1e16ff9802fed88c156d4e0d8d4a2
PierreVelluet <velluetp@gmail.com>
2020-04-27 16:17:44 +0200

    loader done

--- src/components/InMyFridge/NavbarCategories/NavbarCategories.js
+++ src/components/InMyFridge/NavbarCategories/NavbarCategories.js
@@ -3,45 +3,31 @@ import styles from './NavbarCategories.module.css';
 import CategoryIngredient from './CategoryIngredient/CategoryIngredient';
 import axios from 'axios';
 import ingredients from '../../../data/Ingredients';
+import Loader from '../../Assets/CssLoader/Loader';
 
 const NavbarCategories = (props) => {
     const [userInput, setUserInput] = useState("");
-    const [search, setSearch] = useState([]);
     
     const handleChange = (event) => {
         const userValue = event.target.value;
         setUserInput(userValue);
     }
 
-    const handleClick = () => {
-        const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${userInput}&offset=0&number=12`;
-        axios.get(url,
-            {
-              headers: {
-                "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-                "x-rapidapi-key": "788f9512demsh2ae41414a86ef90p1a01bcjsn23eee9f9e33b"
-              }
-            })
-        .then(response => response.data)
-        .then(data => setSearch(data.results))
-        .catch(error => console.log(error));
-    }
-
     const dataFromSearch = () => {
-        props.myCallbackSearch(search);
+        props.myCallbackSearch(userInput);
     }
 
     useEffect(() => {
         dataFromSearch();
-    }, [search])
+    }, [userInput])
 
     
     return (
         <div className={styles.CategoryContainer}>
             <div className={styles.ContainerSearch}>
                 <h2>Make a search</h2>
-                <input placeholder='Your query, ex : "Burger"'type='text' value={userInput} onChange={handleChange} />
-                <button className={styles.buttonCall} onClick={handleClick}>Search a meal</button>
+                <input placeholder='ex : "Burger"'type='text' value={userInput} onChange={handleChange} />
+                <button className={styles.buttonCall} disabled={userInput.length === 0} onClick={props.getRecipeBySearch}>Search a meal</button>
             </div>
             <h2>Choose your ingredient(s)</h2>
             {ingredients.map((element, index) => {
--- src/components/InMyFridge/RecipeList/RecipeList.js
+++ src/components/InMyFridge/RecipeList/RecipeList.js
@@ -1,48 +1,52 @@
 import React, { Component, useState } from 'react';
 import styles from "./RecipeList.module.css";
-import Recipe from './Recipe'
-import axios from 'axios';
+import Recipe from './Recipe';
+import Loader from '../../Assets/CssLoader/Loader';
 
 const RecipeList = (props) => {
-    
-    // const [ top, setTop ] = useState();
-    
-        return (
-            <div className={styles.CardsContainer}>
-                <div className={styles.RecipeList}>
-                    {props.recipeList.map((element, index) => {
+    return (
+        <div className={styles.CardsContainer}>
+            <div className={styles.RecipeList}>
+                {props.recipeList.map((element, index) => {
 
-                        return  <Recipe
-                                    key={index}
-                                    image={element.image}
-                                    title={element.title}
-                                    missedIngredients={element.missedIngredients.slice(0,8).map(
-                                        
-                                      nameingredient => <span> {nameingredient.name}, </span>
-                                    )}
-                                    usedIngredients= {element.usedIngredients.map(
-                                        nameingredient => <span>{nameingredient.name},</span>
-                                    )}
-                                    recipeLikes= {element.likes}
-                                    id={element.id}
-                                />
-                    })}
-                </div>
-                <div className={styles.RecipeList}>
-                    {props.recipeSearch.map((element, index) => {
-
-                        return <Recipe
-                                    key={index}
-                                    image={`https://spoonacular.com/recipeImages/${element.image}`}
-                                    title={element.title}
-                                    readyInMinutes={element.readyInMinutes}
-                                    servings={element.servings}
-                                    id={element.id}
-                                />
-                    })}
-                </div>
+                    return  <Recipe
+                                key={index}
+                                image={element.image}
+                                title={element.title}
+                                missedIngredients={element.missedIngredients.slice(0,8).map(
+                                    
+                                    nameingredient => <span> {nameingredient.name}, </span>
+                                )}
+                                usedIngredients= {element.usedIngredients.map(
+                                    nameingredient => <span>{nameingredient.name},</span>
+                                )}
+                                recipeLikes= {element.likes}
+                                id={element.id}
+                            />
+                })}
             </div>
-        )
+            <div className={styles.ContainerSpinner}>
+                {
+                props.isLoading
+                ? <Loader />
+                : null
+                }
+            </div>
+            <div className={styles.RecipeList}>
+                {props.recipeSearch.map((element, index) => {
+
+                    return <Recipe
+                                key={index}
+                                image={`https://spoonacular.com/recipeImages/${element.image}`}
+                                title={element.title}
+                                readyInMinutes={element.readyInMinutes}
+                                servings={element.servings}
+                                id={element.id}
+                            />
+                })}
+            </div>
+        </div>
+    )
 }
 
 export default RecipeList;
