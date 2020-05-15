042ecf89fbccc299df5399291f8c2ba3392d5892
PierreVelluet <velluetp@gmail.com>
2020-05-15 10:49:21 +0200

    job done

--- src/nodeMailer.js
+++ src/nodeMailer.js
@@ -2,6 +2,7 @@ let express = require('express');
 let router = express.Router();
 let nodemailer = require('nodemailer');
 let cors = require('cors');
+const port = process.env.PORT || 3000;
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -59,4 +60,4 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
-app.listen(3002)
+app.listen(port)
